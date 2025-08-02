e5b3e008524ca044f4321248d52b15b6bdb046b3
Shrishesha Narmatesshvara <shrisheshanarmatesshvara@gmail.com>
2025-08-02 15:09:45 +0000

    still overall is Overall CGPA 0.00

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 import type { Grade } from '@/types';
 
 export const GRADE_POINTS: Record<Grade, number> = {
-  S: 9.5,
-  A: 8.5,
-  B: 7.5,
-  C: 6.5,
-  D: 5.5,
+  S: 10,
+  A: 9,
+  B: 8,
+  C: 7,
+  D: 6,
   F: 0,
   NA: 0,
 };
